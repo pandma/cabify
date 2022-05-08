e600b6d0aca6b432c1d893d0c3c0bd8f182af472
Pablo Andreu <99558817+pandma@users.noreply.github.com>
2022-05-08 14:04:04 +0200

    rondom groups and edit elimate profile

--- routes/eaters.routes.js
+++ routes/eaters.routes.js
@@ -20,6 +20,27 @@ router.get('/getAll',(req,res)=>{
 
 })
 
+router.post('/edit/:eater_Id',(req,res)=>{
+  const{name,email}=req.body
+  const{eater_Id}=req.params
+
+  Eater
+  .findByIdAndUpdate(eater_Id,{name,email})
+  .then((response)=>res.json(response))
+  .catch((err) => res.status(500).json(err));
+
+})
+
+router.post('/:eater_Id/delete',(req,res)=>{
+  const{eater_Id}=req.params
+
+  Eater
+  .findByIdAndRemove(eater_Id)
+  .then(()=>res.json({message:"Eater deleted"}))
+  .catch((err) => res.status(500).json(err));
+
+})
+
 router.post('/delete',(req,res)=>{
 
   Eater
--- routes/group.routes.js
+++ routes/group.routes.js
@@ -4,59 +4,85 @@ const Eaters = require("./../models/Eaters.model");
 const Group = require("./../models/Groups.model");
 
 router.post("/create_groups", (req, res) => {
+  let eaters = []
+  let restaurant = []
 
-  let eaters = [];
-  let restaurant = [];
+  Array.prototype.move = function (from, to) {
+    this.splice(to, 0, this.splice(from, 1)[0]);
+  }
+  // eaters.move(0,Math.floor(Math.random()*(eaters.length)))
 
   Eaters
     .find()
-    .then((response) => {
-      response.map((elm) => eaters.push(elm.name));
-    })
+    .then((response) => response.map((elm) => eaters.push(elm.name)))
     .then(() => {
+
       Restaurant
         .find()
-        .then((response) => {
-          response.map((elm) => restaurant.push(elm.name));
-        })
+        .then((response) => response.map((elm) => restaurant.push(elm.name)))
         .then(() => {
-          let ngroups
-          let sizegroup
+
+          let nGroups
+          let groupSize
           const maxSize = 7
+
           if (eaters.length <= maxSize) {
-            ngroups = 1;
-            sizegroup = maxSize;
+            nGroups = 1
+            groupSize = maxSize;
           } else if (eaters.length % maxSize === 0) {
-            ngroups = eaters.length / maxSize;
-            sizegroup = maxSize;
-          } else if (eaters.length % maxSize-1 === 0) {
-            sizegroup = maxSize-1;
-            ngroups = eaters.length / maxSize-1;
-          } else if (eaters.length % maxSize === 0) {
-            ngroups = eaters.length / maxSize-2;
-            sizegroup = maxSize-2;
-          } else if (eaters.length % maxSize === 0) {
-            ngroups = eaters.length / maxSize-3;
-            sizegroup = maxSize-3;
+            nGroups = eaters.length / maxSize
+            groupSize = maxSize
+          } else if (eaters.length % (maxSize-1) === 0) {
+            groupSize = maxSize-1
+            nGroups = eaters.length / (maxSize-1)
+          } else if (eaters.length % (maxSize-2) === 0) {
+            nGroups = eaters.length / (maxSize-2)
+            groupSize = maxSize-2
+          } else if (eaters.length % (maxSize-3) === 0) {
+            nGroups = eaters.length / (maxSize-3)
+            groupSize = maxSize-3
           } else {
-            ngroups = eaters.length / Math.floor(Math.random()*6)
+            nGroups = eaters.length / Math.floor(Math.random()*6)
           }
-          for (let i = 0; i < ngroups; i++) {
+          for (let i = 0; i < nGroups; i++) {
+            for(let j=1; j<eaters.length;j++){
+            eaters.move(0,j)
+            }
+           
             Group
-             .create({eaters: eaters.slice(0 + i * sizegroup,sizegroup + i * sizegroup),
-              leader: eaters.slice(0 + i * sizegroup, sizegroup + i * sizegroup)
-              [Math.floor(Math.random() * sizegroup)],
-              restaurant: restaurant[Math.floor(Math.random() * restaurant.length)],
-            }).then((response) => console.log(response));
+            .create({eaters: eaters.slice(0 + i * groupSize,groupSize + i * groupSize),
+            leader: eaters.slice(0 + i * groupSize, groupSize + i * groupSize)
+            [Math.floor(Math.random() * groupSize)],
+            restaurant: restaurant[Math.floor(Math.random() * restaurant.length)],
+            })
+            .then((response)=>console.log(response))
+            // .then((response) => res.json(response))
           }
-        });
-    });
-});
+        })
+    })
+})
 
 router.get("/groups", (req, res) => {
-  Group.find()
-    .then((response) => res.json(response))
-    .catch((err) => res.status(500).json(err));
-});
+
+  Group
+  .find()
+  .then((group)=>{
+    if(group.length===0){
+      res.status(401).json({ message: "group not created yet" })
+      }else{
+      res.json(group)
+    }
+    })
+  .catch((err) => res.status(500).json(err))
+})
+
+router.get("/leaders/:leader_Id", (req, res) => {
+  const {leader_Id} = req.params
+
+  Group
+  .findById(leader_Id)
+  .then((group)=>res.json(group))
+  .catch((err) => res.status(500).json(err))
+})
 
 module.exports = router;
